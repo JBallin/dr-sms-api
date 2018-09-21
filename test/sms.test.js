f290dad65ab40686d98e9730aa6e7818007c28da
JBallin <JBallin@users.noreply.github.com>
2018-09-21 14:22:17 -0700

    Write tests for createApptMsg()

---
+++ test/sms.test.js
@@ -0,0 +1,22 @@
+const { assert } = require('chai');
+const { createApptMsg } = require('../routes/sms');
+
+describe('sms', () => {
+  describe('createApptMsg', () => {
+    it('should return appointment message', () => {
+      assert.equal(
+        createApptMsg('Abushadi', 'J Ballin', '12/20/18', '10am'),
+        'Hello J Ballin, this is Dr. Abushadi\'s office reminding you about '
+        + 'your appointment on 12/20/18 at 10am.',
+      );
+    });
+
+    it('should return appointment message with a custom message', () => {
+      assert.equal(
+        createApptMsg('Abushadi', 'J Ballin', '12/20/18', '10am', 'Please bring your x-rays!'),
+        'Hello J Ballin, this is Dr. Abushadi\'s office reminding you about '
+        + 'your appointment on 12/20/18 at 10am.\nNOTE: Please bring your x-rays!',
+      );
+    });
+  });
+});
